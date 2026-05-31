a17b37806ecca8d2e439b954f613950ba72c9e2a
Ari Weinberg <ari@ari.com>
2026-05-31 05:03:30 +0300

    minimize root level code.

--- dynamic_shapes/__init__.py
+++ dynamic_shapes/__init__.py
@@ -3,15 +3,18 @@ import importlib
 from app_logger import get_logger
 
 
-logger = get_logger("dynamic_shapes")
+def load_all_shapes():
+    logger = get_logger("dynamic_shapes")
 
-for file_name in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+    for file_name in os.listdir(os.path.dirname(os.path.abspath(__file__))):
 
-    if file_name.endswith(".py") and file_name != os.path.basename(__file__):
-        module_name = file_name[:-3]
-        try:
-            # Use relative import notation (note the leading dot) and pass the package name
-            importlib.import_module(f".{module_name}", package="dynamic_shapes")
-            logger.info(f"Successfully imported: {module_name}")
-        except Exception as e:
-            logger.warning(f"Failed to import {module_name}: {e}")
+        if file_name.endswith(".py") and file_name != os.path.basename(__file__):
+            module_name = file_name[:-3]
+            try:
+                # Use relative import notation (note the leading dot) and pass the package name
+                importlib.import_module(f".{module_name}", package="dynamic_shapes")
+                logger.info(f"Successfully imported: {module_name}")
+            except Exception as e:
+                logger.warning(f"Failed to import {module_name}: {e}")
+
+load_all_shapes()
